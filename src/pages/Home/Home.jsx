c6cdf7d6aa1d1b98bfbb7992cbd0c62c838f5787
Samruddhi Virkhare <samruddhi.virkhare@cumminscollege.edu.in>
2024-08-04 02:34:32 +0530

    Create Home.jsx
    The Home component serves as the main entry point for displaying the list of projects in the application. It utilizes the ProjectList component to render and manage the project data.

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProjectList from '../ProjectList/ProjectList'
+const Home = () =>{
+    return(
+        <div>
+            <ProjectList/>
+        </div>
+    )
+}
+
+export default Home
